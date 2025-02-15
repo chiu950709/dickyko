9e0e223b058ac15a8ac303cf9a129fade0507bb6
chiu950709 <chiu950709@gmail.com>
2025-02-14 22:46:00 -0700

    update css, add exp, add about

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 function About() {
+    //<a href="https://www.thepeak.com.hk/en" target="_blank" rel="noopener noreferrer" >Hong Kong Peak Tramways Company Limited</a>
     return (
         <section id="about">
             <h2 class='pageHeader'>About Me</h2>
             <iframe title="myFrame" src="https://drive.google.com/file/d/1epa1OXmBbBTNYVwZq4DpWpZaKroNQaVx/preview" width="250" height="400" allow="autoplay"></iframe>
-            <p class="intro">I am <b>Dicky Ko</b> from Hong Kong, currently base in Calgary, AB, Canada. A passionate full stack web developer with 5 years of experience in maintaining and enhancing the existing web applications
+            <p class="intro">I am <code>Dicky Ko</code> from Hong Kong, currently base in Calgary, AB, Canada.</p> 
+            <p class="intro">A passionate <code>full stack web developer</code> with 5 years of experience in maintaining and enhancing the existing web applications
                 and creating website application. I have experience working with and have solid understanding in various frontend and backend frameworks. Strong problem-solving skills and able to work collaboratively in a team environment. 
                 I’m eager to continue learning new technologies and adapting to new challenges.</p>
+            <p class="intro">An wistful <code>customer Service Assistant</code> with 2 years of experiecnce in handling inquiries, resolving issues, and ensuring customer satisfaction. </p>
             <h2 class='pageHeader'>Education</h2>
             <div class="education">
                 <h3>The Open University of Hong Kong
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 function Experience() {
-    const openPopup = (url) => {
+    /*const openPopup = (url) => {
         window.open(url, 'popup');
-    };
+    };*/
 
     return (
         <section id="experience">
             <h2 class="pageHeader">Work Experience</h2>
-
+            <h2 class="pageHeader">Relevant IT Experience</h2>
             <div class="experience">
                 <h3>Digital Policy Office(Former name: Office of the Government Chief Information Officer) 
                     <code>Oct 2023 – Oct 2024</code>
@@ -56,8 +56,11 @@ function Experience() {
             <div class="experience">
                 <h3>Cognizant 
                     <code>June 2022 – Oct 2023</code>
-                    <a href="#!" onClick={() => openPopup('https://www.aig.com/home')}>
-                    <img src="https://www.aig.com/content/experience-fragments/aig/america-canada/us_aig/en/header/master/_jcr_content/root/responsivegrid_1958828262/responsivegrid_copy/container_copy_copy/container_897891850/image_409587976.coreimg.png/1677180907520/icon-aig-logo-white.png" alt="Lamp" width="89" height="48"/></a>
+                    {
+                    //<a href="#!" onClick={() => openPopup('https://www.aig.com/home')}>
+                    }
+                    <a href="https://www.aig.com/home'" target="_blank" rel="noopener noreferrer" >
+                    <img src="https://www.aig.com/content/experience-fragments/aig/america-canada/us_aig/en/header/master/_jcr_content/root/responsivegrid_1958828262/responsivegrid_copy/container_copy_copy/container_897891850/image_409587976.coreimg.png/1677180907520/icon-aig-logo-white.png" alt="AIG" width="89" height="48"/></a>
                 </h3>
                 <div class="detail"><code class="detail">
                     &#123;
@@ -130,10 +133,55 @@ function Experience() {
                     &#125;
                 </code></div>
                 <h3>Public web services maintained and developed during working experience</h3>
-                <a href="#!" class="link-style" onClick={() => openPopup('https://www.eapp.gov.hk/en/')}>EAPP</a>&emsp;&emsp; 
-                <a href="#!" class="link-style" onClick={() => openPopup('https://www.cspe.edu.hk/en/')}>Concourse</a>&emsp;&emsp;
-                <a href="#!" class="link-style" onClick={() => openPopup('https://feddcs.cspe.edu.hk/')}>FEDDCS</a>&emsp;&emsp;
-                <a href="#!" class="link-style" onClick={() => openPopup('https://www.vpet.edu.hk/en/')}>VPET</a>    
+                {
+                //<a href="#!" class="link-style" onClick={() => openPopup('https://www.eapp.gov.hk/en/')}>EAPP</a>&emsp;&emsp; 
+                //<a href="#!" class="link-style" onClick={() => openPopup('https://www.cspe.edu.hk/en/')}>Concourse</a>&emsp;&emsp;
+                //<a href="#!" class="link-style" onClick={() => openPopup('https://feddcs.cspe.edu.hk/')}>FEDDCS</a>&emsp;&emsp;
+                //<a href="#!" class="link-style" onClick={() => openPopup('https://www.vpet.edu.hk/en/')}>VPET</a>  
+                }
+                <a href="https://www.eapp.gov.hk/en/" class="link-style" target="_blank" rel="noopener noreferrer" >EAPP</a>&emsp;&emsp; 
+                <a href="https://www.cspe.edu.hk/en/" class="link-style" target="_blank" rel="noopener noreferrer" >Concourse</a>&emsp;&emsp;
+                <a href="https://feddcs.cspe.edu.hk/" class="link-style" target="_blank" rel="noopener noreferrer" >FEDDCS</a>&emsp;&emsp;
+                <a href="https://www.vpet.edu.hk/en/" class="link-style" target="_blank" rel="noopener noreferrer" >VPET</a> 
+            </div> 
+
+            <h2 class="pageHeader">Relevant Customer Service Experience</h2>
+            <div class="experience">
+                <h3>Peak Tramways Company Limited
+                    <code>Aug 2022 – Sep 2024</code>
+                </h3>
+                <a href="https://www.thepeak.com.hk/en" target="_blank" rel="noopener noreferrer" >
+                <img src="https://www.thepeak.com.hk/sites/peak/files/2025-01/The%20peak%20tram_option%202_ENG.png" alt="PeakTram" width="1000" height="450"/></a>
+                <div class="detail"><code class="detail">
+                    &#123;
+                    <div>
+                        <p>
+                            &emsp;&emsp;<span class="jobTitle">"Title"</span>&#58;&nbsp;
+                            <span class="jobValue">"Customer Service Assistant"</span>
+                            <div></div>
+                            &emsp;&emsp;<span class="jobDuties">"Job Duties"</span>&#58;&nbsp;&#91;
+                            <br></br>
+                            <span class="jobValue">
+                                &emsp;&emsp;&emsp;&emsp;"Developed a web application and assisted in development of the REST API"
+                            </span>,
+                            <br></br>
+                            <span class="jobValue">
+                                &emsp;&emsp;&emsp;&emsp;"Assist customers with payment transactions at service counters, sales of products"
+                            </span>,
+                            <br></br>
+                            <span class="jobValue">
+                                &emsp;&emsp;&emsp;&emsp;"Respond to customer inquiries, complaints, and feedback, providing effective solutions"
+                            </span>,
+                            <br></br>
+                            <span class="jobValue">
+                                &emsp;&emsp;&emsp;&emsp;"Manage crowd control to ensure passenger safety and enhance the overall customer experience."
+                            </span>
+                            <br></br>
+                            &emsp;&emsp;&#93;
+                        </p>
+                    </div>
+                    &#125;
+                </code></div>
             </div> 
         </section>
     );
