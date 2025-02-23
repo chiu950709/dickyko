44bb025e638f45c8fbfaf09edede3acb0c336115
chiu950709 <chiu950709@gmail.com>
2025-02-23 15:46:04 -0700

    relocate link to config

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import config from '../config';
 
 function Sidebar() {
   /*const openPopup = (url) => {
@@ -18,9 +19,9 @@ function Sidebar() {
           //<li><a href="#!" onClick={() => openPopup('https://drive.google.com/file/d/1iG5CH5W3yYwtPuRYQbAjfxGoU-E1sXtw/view?usp=sharing')}>Resume</a></li>
           //<li><a href="#!" onClick={() => openPopup('https://github.com/chiu950709?tab=repositories')}>GitHub</a></li>
           }
-          <li><a href="https://www.linkedin.com/in/dicky-ko-493053234/" target="_blank" rel="noopener noreferrer" >Linkedin</a></li>
-          <li><a href="https://drive.google.com/file/d/1iG5CH5W3yYwtPuRYQbAjfxGoU-E1sXtw/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a></li>
-          <li><a href="https://github.com/chiu950709?tab=repositories" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+          <li><a href={`${config.LINKEDIN}`} target="_blank" rel="noopener noreferrer" >Linkedin</a></li>
+          <li><a href={`${config.RESUME}`} target="_blank" rel="noopener noreferrer">Resume</a></li>
+          <li><a href={`${config.GITHUB}`} target="_blank" rel="noopener noreferrer">GitHub</a></li>
         </ol>
       </nav>
     </header>
--- src/config.js
+++ src/config.js
@@ -2,6 +2,9 @@ import selfPhoto from './img/selfphoto.jpeg';
 
 const config = {
   SELF_IMG_PATH: selfPhoto, 
+  LINKEDIN: 'https://www.linkedin.com/in/dicky-ko-493053234/',
+  RESUME: 'https://drive.google.com/file/d/1iG5CH5W3yYwtPuRYQbAjfxGoU-E1sXtw/view?usp=sharing',
+  GITHUB: 'https://github.com/chiu950709?tab=repositories',
 };
 
 export default config;
